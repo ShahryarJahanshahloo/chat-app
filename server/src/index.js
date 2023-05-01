b37545e2824301346bce31e725937d1862e3d98e
shahryar jahanshahloo <shahryar.jahanshahloo@gmail.com>
2023-05-01 11:45:21 +0330

    added prisma client

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,10 @@
 import http from 'http'
 import app from './app.js'
 import { Server } from 'socket.io'
+import { PrismaClient } from '@prisma/client'
 
 const server = http.createServer(app)
-
+const prisma = new PrismaClient()
 const io = new Server(server, {
   cors: {
     origin: 'http://localhost:3000',
